96c64cd652144a1787d165e8e3df4d62b9c1f44d
Vesel4ak31 <gushchinkv@yandex.ru>
2026-09-12 23:09:52 +0300

    Add title information for Virus Analyzer Tool

---
+++ config/title.py
@@ -0,0 +1,8 @@
+title = r"""
+____   ____.__               __          
+\   \ /   /|__|____    _____/  |_  ____   Virus  Analyzer Tool
+ \   Y   / |  \__  \  /    \   __\/  _ \  Made by Vesel4ak31
+  \     /  |  |/ __ \|   |  \  | (  <_> ) Github: https://githib.com/Vesel4ak31/Vianto
+   \___/   |__(____  /___|  /__|  \____/  
+                   \/     \/             
+        """
